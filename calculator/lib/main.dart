666e524c6fdae429d1e49457ad744fc64ff8ec8b
Allan Dale Ho <allandale.ho@gmail.com>
2018-07-16 13:00:22 +0800

    commit

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Calculator App 3",
+      title: "Calculator App 4",
       theme: new ThemeData(primarySwatch: Colors.red),
       home: HomePage(),
     );
